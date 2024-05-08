88c987d99be5a23ae3ca02f4a124e83efb4e61c8
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-05-08 12:24:56 -0300

    Update authController.js

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -4,9 +4,9 @@ exports.authenticateFacebook = passport.authenticate('facebook');
 exports.authenticateGoogle = passport.authenticate('google', { scope: ['profile'] });
 
 exports.facebookCallback = passport.authenticate('facebook', { failureRedirect: '/' }, (req, res) => {
-    res.send('Autenticado com sucesso via Facebook!');
+    res.send('Successfully authenticated via Facebook');
 });
 
 exports.googleCallback = passport.authenticate('google', { failureRedirect: '/' }, (req, res) => {
-    res.send('Autenticado com sucesso via Google!');
+    res.send('Successfully authenticated via Google');
 });
