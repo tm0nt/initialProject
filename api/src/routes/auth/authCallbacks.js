1ed2be1dec49a9efe29b52d6368fd7b81627f655
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-05-08 12:29:52 -0300

    Update authCallbacks.js

--- api/src/routes/auth/authCallbacks.js
+++ api/src/routes/auth/authCallbacks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../../controllers/authController');
 
-router.get('/facebook/callback', authController.facebookCallback);
-router.get('/google/callback', authController.googleCallback);
+router.get('/auth/facebook/callback', authController.facebookCallback);
+router.get('/auth/google/callback', authController.googleCallback);
 
 module.exports = router;
